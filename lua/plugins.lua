7c2d615f2daa043e3b084f95871bd033554d628f
Shamash2014 <warp.buddhist@gmail.com>
2025-07-08 22:35:02 +0300

    fix(oild): oild fix

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,8 +4,100 @@ return {
     lazy = true,
     opts = {
       default_file_explorer = true,
+      buf_options = {
+        buflisted = false,
+        bufhidden = "hide",
+      },
+      win_options = {
+        wrap = false,
+        signcolumn = "no",
+        cursorcolumn = false,
+        foldcolumn = "0",
+        spell = false,
+        list = false,
+        conceallevel = 3,
+        concealcursor = "nvic",
+      },
+      delete_to_trash = true,
+      skip_confirm_for_simple_edits = true,
+      prompt_save_on_select_new_entry = true,
+      cleanup_delay_ms = 2000,
+      lsp_file_methods = {
+        timeout_ms = 1000,
+        autosave_changes = false,
+      },
+      constrain_cursor = "editable",
+      watch_for_changes = true,
+      use_default_keymaps = true,
       view_options = {
         show_hidden = true,
+        is_hidden_file = function(name, bufnr)
+          return vim.startswith(name, ".")
+        end,
+        is_always_hidden = function(name, bufnr)
+          return false
+        end,
+        natural_order = true,
+        case_insensitive = false,
+        sort = {
+          { "type", "asc" },
+          { "name", "asc" },
+        },
+      },
+      extra_scp_args = {},
+      git = {
+        add = function(path)
+          return false
+        end,
+        mv = function(src_path, dest_path)
+          return false
+        end,
+        rm = function(path)
+          return false
+        end,
+      },
+      float = {
+        padding = 2,
+        max_width = 90,
+        max_height = 0,
+        border = "rounded",
+        win_options = {
+          winblend = 0,
+        },
+        get_win_title = nil,
+        preview_split = "auto",
+        override = function(conf)
+          return conf
+        end,
+      },
+      preview = {
+        max_width = 0.9,
+        min_width = { 40, 0.4 },
+        width = nil,
+        max_height = 0.9,
+        min_height = { 5, 0.1 },
+        height = nil,
+        border = "rounded",
+        win_options = {
+          winblend = 0,
+        },
+        update_on_cursor_moved = true,
+      },
+      progress = {
+        max_width = 0.9,
+        min_width = { 40, 0.4 },
+        width = nil,
+        max_height = { 10, 0.9 },
+        min_height = { 5, 0.1 },
+        height = nil,
+        border = "rounded",
+        minimized_border = "none",
+        win_options = {
+          winblend = 0,
+        },
+      },
+      ssh = {
+        border = "rounded",
       },
       keymaps = {
         ["g?"] = "actions.show_help",
@@ -23,6 +115,9 @@ return {
         ["gs"] = "actions.change_sort",
         ["gx"] = "actions.open_external",
         ["g."] = "actions.toggle_hidden",
+      },
+      keymaps_help = {
+        border = "rounded",
       },
     },
     keys = {
