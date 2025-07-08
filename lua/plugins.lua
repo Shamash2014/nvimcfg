fd4ceef28980d7540be637d3291d543756942a8e
Shamash2014 <warp.buddhist@gmail.com>
2025-07-08 17:59:32 +0300

    fix(blink): fix bloink

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -135,101 +135,93 @@ return {
     end,
   },
   {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
+    "saghen/blink.cmp",
+    lazy = false,
     dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer", 
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
-      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+      "L3MON4D3/LuaSnip",
       "supermaven-inc/supermaven-nvim",
       "tzachar/cmp-tabnine",
-      "L3MON4D3/LuaSnip",
-      "rafamadriz/friendly-snippets",
+      "saghen/blink.compat",
     },
-    config = function()
-      local cmp = require("cmp")
-      local luasnip = require("luasnip")
-      
-      -- Load friendly snippets
-      require("luasnip.loaders.from_vscode").lazy_load()
-      
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
+    version = "v0.*",
+    opts = {
+      keymap = {
+        preset = 'default',
+        ['<C-j>'] = { 'select_next', 'fallback' },
+        ['<C-l>'] = { 'select_prev', 'fallback' },
+        ['<CR>'] = { 'accept', 'fallback' },
+        ['<Tab>'] = { 'snippet_forward', 'fallback' },
+        ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+      },
+      appearance = {
+        use_nvim_cmp_as_default = true,
+        nerd_font_variant = 'mono'
+      },
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer', 'supermaven', 'cmp_tabnine' },
+        cmdline = {},
+        providers = {
+          supermaven = {
+            name = "supermaven",
+            module = "blink.compat.source",
+            score_offset = 100,
+            opts = {
+              get_sources = function()
+                return { require("supermaven-nvim.cmp") }
+              end,
+            },
+          },
+          cmp_tabnine = {
+            name = "cmp_tabnine", 
+            module = "blink.compat.source",
+            score_offset = 90,
+            opts = {
+              get_sources = function()
+                return { require("cmp_tabnine.source") }
+              end,
+            },
+          },
         },
-        mapping = cmp.mapping.preset.insert({
-          ['<C-j>'] = cmp.mapping.select_next_item(),
-          ['<C-l>'] = cmp.mapping.select_prev_item(),
-          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
-          ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-          ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-        }),
-        sources = cmp.config.sources({
-          { name = 'nvim_lsp', priority = 1000 },
-          { name = 'supermaven', priority = 900 },
-          { name = 'cmp_tabnine', priority = 800 },
-          { name = 'luasnip', priority = 700 },
-          { name = 'buffer', priority = 500 },
-          { name = 'path', priority = 400 },
-        }),
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
+      },
+      completion = {
+        accept = {
+          auto_brackets = {
+            enabled = true,
+          },
         },
-        formatting = {
-          format = function(entry, vim_item)
-            vim_item.menu = ({
-              nvim_lsp = "[LSP]",
-              supermaven = "[Supermaven]",
-              cmp_tabnine = "[TabNine]", 
-              luasnip = "[Snippet]",
-              buffer = "[Buffer]",
-              path = "[Path]",
-            })[entry.source.name]
-            return vim_item
-          end,
+        menu = {
+          draw = {
+            treesitter = { "lsp" },
+            columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind" } },
+          },
         },
-      })
-      
-      -- Command line completion
-      cmp.setup.cmdline(':', {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-          { name = 'path' }
-        }, {
-          { name = 'cmdline' }
-        })
-      })
-    end,
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 200,
+        },
+      },
+      snippets = {
+        expand = function(snippet)
+          require('luasnip').lsp_expand(snippet)
+        end,
+        active = function(filter)
+          if filter and filter.direction then
+            return require('luasnip').jumpable(filter.direction)
+          end
+          return require('luasnip').in_snippet()
+        end,
+        jump = function(direction)
+          require('luasnip').jump(direction)
+        end,
+      },
+    },
+    opts_extend = { "sources.default" }
   },
   {
     "tzachar/cmp-tabnine",
     lazy = true,
-    event = "InsertEnter",
+    event = "InsertEnter", 
     build = "./install.sh",
     config = function()
       require('cmp_tabnine.config'):setup({
